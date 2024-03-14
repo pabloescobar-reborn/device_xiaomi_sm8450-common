f76c98f89a5186df7943692667fa69629fe72ad0
bheatleyyy <sugakesshaa@pixelos.net>
2024-03-14 09:08:32 +0100

    sm8450-common: Import Dolby Atmos Changes
    Change-Id: 3ddf55ed97e80cf6256ddf82bfc29b0cd0115cf3
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Co-Authored-By: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-Authored-By: Ivan Martynov <johnivan19999@gmail.com>
    Co-Authored-By: ∂α૨ҡ_ρɦσεɳเא <80503611+drkphnx@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -324,6 +324,13 @@ PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.mlipay@1.1.vendor \
     vendor.xiaomi.hardware.mtdservice@1.0.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
+    $(LOCAL_PATH)/media/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml
+
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
+
 # Network
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
