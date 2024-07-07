c780648126b33c4fac8aa428614f451b9c11b833
flakeforever <113103193+flakeforever@users.noreply.github.com>
2024-07-07 17:37:23 +0800

    sm8450-common: Build libcodec2_hidl_shim

--- common.mk
+++ common.mk
@@ -263,6 +263,7 @@ PRODUCT_PACKAGES += \
     libcodec2_hidl@1.0.vendor \
     libcodec2_soft_common.vendor \
     libsfplugin_ccodec_utils.vendor \
+    libcodec2_hidl_shim.vendor \
 
 # Network
 PRODUCT_COPY_FILES += \
