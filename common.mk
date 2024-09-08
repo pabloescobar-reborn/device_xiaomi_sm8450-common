6b11b1b8a329ca0d2f156423dd2e917e7d2a852f
Jens Reidel <adrian@travitia.xyz>
2024-09-08 16:45:59 +0200

    sm8450-common: Commonize udfps doze sensor type
    This is only applied if our udfps sensor prop is matched on.
    
    Change-Id: I6fb008fad2c6b4b4a5b844fbec72e026fad0f03c
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- common.mk
+++ common.mk
@@ -376,6 +376,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     DialerResXiaomi \
     FrameworksResTarget \
+    FrameworksResUdfpsSensor \
     FrameworksResXiaomi \
     LineageResXiaomi \
     SettingsProviderResXiaomi \
