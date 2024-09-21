858d29092747066cef2e583cbe258e3d9933b7e1
Kyuofox <kyuofox@gmail.com>
2024-09-21 23:00:44 +0530

    sm8450-common: Replace libwvhidl with v34 libcrypto
    On Android 15 Release 1, it will cause the problem of "Cannot find symbol "CBS_init"", This issue occurs on older devices using "android.hardware.drm@1.3-service.widevine", Because of vndk on vendor, see ref[1], "CBS_init" becomes a missing symbol, We can't revert changes, that would cause more things to not work, So replace it with libcrypto from vndk 34, to fix this issue.
    
    [1]: https://android.googlesource.com/platform/system/core/+/ad8cf52c4c2b60250d52f1b0b90648fdf407dff4
    
    Signed-off-by: Kyuofox <kyuofox@gmail.com>
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- extract-files.sh
+++ extract-files.sh
@@ -111,6 +111,9 @@ function blob_fixup() {
         *)
             return 1
             ;;
+        vendor/lib64/libwvhidl.so)
+            "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v34.so" "${2}"
+            ;; 
     esac
 }
 
