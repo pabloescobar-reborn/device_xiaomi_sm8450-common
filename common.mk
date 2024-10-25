a91d00588d4f2f92bf51b7a795fe6f597b0afbf8
Arian <arian.kulmer@web.de>
2024-10-25 15:30:27 +0200

    Revert "sm8450-common: Commonize udfps doze sensor type"
    The prop is set too late..
    
    This reverts commit 1c043b528c11ea3c1990c88854af3b62ec83fb35.

--- common.mk
+++ common.mk
@@ -301,7 +301,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     DialerResXiaomi \
     FrameworksResTarget \
-    FrameworksResUdfpsSensor \
     FrameworksResXiaomi \
     LineageResXiaomi \
     SettingsProviderResXiaomi \
